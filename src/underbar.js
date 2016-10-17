347417812b130fdae301a0e986a9853816f7efe5
Nicholas Wang <nickwang55@gmail.com>
2016-10-17 22:33:16 +0800

    Implemented _.some

--- src/underbar.js
+++ src/underbar.js
@@ -217,6 +217,13 @@
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if(iterator === undefined){ iterator = _.identity; }
+    return _.reduce(collection, function(pass, item){
+      if(pass){
+        return true;
+      }
+      return Boolean(iterator(item));
+    }, false);
   };
 
 
